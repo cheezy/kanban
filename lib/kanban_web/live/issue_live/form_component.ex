4e0ce56754d0eec1aa85c589936088eb49ddb8b3
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-11-29 18:15:31 -0500

    adjusted height of select

--- lib/kanban_web/live/issue_live/form_component.ex
+++ lib/kanban_web/live/issue_live/form_component.ex
@@ -61,7 +61,7 @@ defmodule KanbanWeb.IssueLive.FormComponent do
                 id="issue_title"
                 value={@form[:title].value}
                 required
-                class={"w-full px-3 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 #{if @form[:title].errors != [], do: "border-red-500", else: "border-gray-300"}"}
+                class={"w-full px-3 py-2 h-10 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 #{if @form[:title].errors != [], do: "border-red-500", else: "border-gray-300"}"}
                 placeholder={gettext("Brief description of the issue")}
               />
               <%= for error <- @form[:title].errors do %>
@@ -76,7 +76,7 @@ defmodule KanbanWeb.IssueLive.FormComponent do
               <select
                 name="issue[label]"
                 id="issue_label"
-                class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                class="w-full px-3 py-2 h-10 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
               >
                 <%= for {display, value} <- label_options() do %>
                   <option value={value} selected={@form[:label].value == value}>
