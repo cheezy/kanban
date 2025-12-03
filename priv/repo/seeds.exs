b4fa41887ceb36d02416121b9dbad4d15c501183
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-12-03 13:26:29 -0500

    added seeds file to seed a couple of users

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,19 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Kanban.Accounts
+
+Accounts.register_user(%{
+  name: "Cheezy Morgan",
+  email: "cheezy@letstango.ca",
+  password: "change_me_cheezy",
+  type: :admin
+})
+
+Accounts.register_user(%{
+  name: "Ardita Karaj",
+  email: "ardita@letstango.ca",
+  password: "change_me_ardita",
+  type: :admin
+})
