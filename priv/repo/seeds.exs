d402fad2376e46fc2520705cb99b2a4dafc8a346
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-12-04 18:36:06 -0500

    updating users to admin

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,18 +10,30 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+alias Kanban.Accounts.User
 alias Kanban.Accounts
+alias Kanban.Repo
 
-Accounts.register_user(%{
-  name: "Cheezy Morgan",
-  email: "cheezy@letstango.ca",
-  password: "change_me_cheezy",
-  type: :admin
-})
+user1 = Accounts.get_user_by_email("cheezy@letstango.ca")
+user2 = Accounts.get_user_by_email("ardita@letstango.ca")
+# {:ok, user1} =
+#   Accounts.register_user(%{
+#     name: "Cheezy Morgan",
+#     email: "cheezy@letstango.ca",
+#     password: "change_me_cheezy"
+#   })
 
-Accounts.register_user(%{
-  name: "Ardita Karaj",
-  email: "ardita@letstango.ca",
-  password: "change_me_ardita",
-  type: :admin
-})
+user1
+|> Ecto.Changeset.change(type: :admin)
+|> Repo.update!()
+
+# {:ok, user2} =
+#   Accounts.register_user(%{
+#     name: "Ardita Karaj",
+#     email: "ardita@letstango.ca",
+#     password: "change_me_ardita"
+#   })
+
+user2
+|> Ecto.Changeset.change(type: :admin)
+|> Repo.update!()
