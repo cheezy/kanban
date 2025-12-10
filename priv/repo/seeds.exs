247ed0db895acc4775a10ef4cd5c451c1c51d66b
Jeff Morgan <cheezy@industriallogic.com>
2025-12-10 14:08:40 -0500

    Accessibility and updated a few dependencies. (#7)
    * added back to app link
    
    * added back to app link for dashboard
    
    * translations for new links
    
    * fixing a few credo and sobelow issues
    
    * updated a few dependencies
    
    * removed temporary seeds
    
    * fixed some accessibility issues
    
    * fixing access
    
    * removed back links on dashboard and error tracker

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,28 +9,3 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-
-alias Kanban.Accounts
-alias Kanban.Repo
-
-{:ok, user1} =
-  Accounts.register_user(%{
-    name: "Cheezy Morgan",
-    email: "cheezy@letstango.ca",
-    password: "change_me_cheezy"
-  })
-
-user1
-|> Ecto.Changeset.change(type: :admin)
-|> Repo.update!()
-
-{:ok, user2} =
-  Accounts.register_user(%{
-    name: "Ardita Karaj",
-    email: "ardita@letstango.ca",
-    password: "change_me_ardita"
-  })
-
-user2
-|> Ecto.Changeset.change(type: :admin)
-|> Repo.update!()
