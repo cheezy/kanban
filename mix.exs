9c2d136bfaafba95b3a4ad8eb508ff90c3f7b768
Jeffrey S. Morgan <cheezy@industriallogic.com>
2025-12-10 13:51:29 -0500

    removed back links on dashboard and error tracker

--- mix.exs
+++ mix.exs
@@ -7,9 +7,6 @@ defmodule Kanban.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
-      elixirc_options: [
-        ignore_module_conflict: true
-      ],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
@@ -107,7 +104,6 @@ defmodule Kanban.MixProject do
   defp test_coverage do
     [
       ignore_modules: [
-        ErrorTracker.Web.Layouts,
         Kanban.AccountsFixtures,
         Kanban.Application,
         Kanban.BoardsFixtures,
@@ -125,8 +121,7 @@ defmodule Kanban.MixProject do
         KanbanWeb.PageHTML,
         KanbanWeb.Plugs.Locale,
         KanbanWeb.Router,
-        KanbanWeb.Telemetry,
-        Phoenix.LiveDashboard.LayoutView
+        KanbanWeb.Telemetry
       ]
     ]
   end
