a165aa4aaa2aefca969687c3874d2c520a75c34f
Jeffrey S. Morgan <cheezy@industriallogic.com>
2026-01-14 15:23:02 -0500

    trying to get telemetry working

--- lib/kanban_web/router.ex
+++ lib/kanban_web/router.ex
@@ -85,7 +85,7 @@ defmodule KanbanWeb.Router do
     pipe_through [:browser, :require_authenticated_user, :require_admin_user]
 
     live_dashboard "/dashboard",
-      metrics: {KanbanWeb.Telemetry, :metrics},
+      metrics: KanbanWeb.Telemetry,
       ecto_repos: [Kanban.Repo],
       ecto_psql_extras_options: [long_running_queries_threshold: [threshold: "200 milliseconds"]]
 
