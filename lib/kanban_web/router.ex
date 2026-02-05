7d19cc39a0b69200c2ba6b60aa46e64c59834755
Jeffrey S. Morgan <cheezy@industriallogic.com>
2026-02-05 18:52:32 -0500

    Completed task W23: Create Wait Time drill-down page

--- lib/kanban_web/router.ex
+++ lib/kanban_web/router.ex
@@ -120,6 +120,7 @@ defmodule KanbanWeb.Router do
       live "/boards/:id/metrics/throughput", MetricsLive.Throughput, :throughput
       live "/boards/:id/metrics/cycle-time", MetricsLive.CycleTime, :cycle_time
       live "/boards/:id/metrics/lead-time", MetricsLive.LeadTime, :lead_time
+      live "/boards/:id/metrics/wait-time", MetricsLive.WaitTime, :wait_time
       live "/boards/:id/archive", ArchiveLive.Index, :index
       live "/boards/:id/api_tokens", BoardLive.Show, :api_tokens
       live "/boards/:id/columns/new", BoardLive.Show, :new_column
