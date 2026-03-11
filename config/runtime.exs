9a4745ea4575b0d88d5db116568f273c5e447670
Jeffrey S. Morgan <cheezy@industriallogic.com>
2026-03-11 16:00:07 -0400

    fixing well known issue with postgres connections and timeouts

--- config/runtime.exs
+++ config/runtime.exs
@@ -45,7 +45,8 @@ if config_env() == :prod do
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
     # pool_count: 4,
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    disconnect_on_error_codes: [:fatal_postmaster, :closed]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
