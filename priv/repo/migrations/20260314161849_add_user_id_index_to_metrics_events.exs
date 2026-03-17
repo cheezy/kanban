d9582875556182e2af3588155deda9ff6601f366
Jeffrey S. Morgan <cheezy@industriallogic.com>
2026-03-17 10:43:02 -0400

    Completed task W83: Remove title, type, and description from stride-enriching-tasks enrichment phases

--- priv/repo/migrations/20260314161849_add_user_id_index_to_metrics_events.exs
+++ priv/repo/migrations/20260314161849_add_user_id_index_to_metrics_events.exs
@@ -3,7 +3,7 @@ defmodule Kanban.Repo.Migrations.AddUserIdIndexToMetricsEvents do
 
   def change do
     create index(:metrics_events, ["((metadata->>'user_id'))"],
-      name: :metrics_events_metadata_user_id_index
-    )
+             name: :metrics_events_metadata_user_id_index
+           )
   end
 end
