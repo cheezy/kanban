38b8f2d017871adb0fa1a374517c42e6f7da6c6b
Jeffrey S. Morgan <cheezy@industriallogic.com>
2026-06-26 19:39:27 -0400

    in the process of upgrading to elixir 1.20

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Kanban.MixProject do
     [
       app: :kanban,
       version: "1.25.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
